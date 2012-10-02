6dcafd51362d4dc01993affa01880756207854e7
TJ Biddle <tjbiddle@inflection.com>
2012-10-01 18:54:09 -0700

    adding underscores_in_headers on; to the nginx config template

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -25,7 +25,7 @@ module PhusionPassenger
 	###### Version numbers ######
 	
 	# Phusion Passenger version number. Don't forget to edit ext/common/Constants.h too.
-	VERSION_STRING = '3.0.17'
+	VERSION_STRING = '3.0.18'
 	
 	PREFERRED_NGINX_VERSION = '1.2.3'
 	PREFERRED_PCRE_VERSION  = '8.31'
